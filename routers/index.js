1536cc42a3adc42f912c38962d847d9652d0fde4
rioadistya <rioputra191101@gmail.com>
2022-09-23 10:17:16 +0700

    adasfa

--- routers/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const usersRouter = require('./usersRouter.js');
-const reflectionsRouter = require('./reflectionsRouter');
-
-router.use('/api/v1/users',usersRouter);
-router.use('/api/v1/reflections',reflectionsRouter);
-
-module.exports = router;
