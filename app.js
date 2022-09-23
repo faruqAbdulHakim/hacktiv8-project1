c29efac0c5ec913a6069690e49676ec3b11e6bde
rioadistya <rioputra191101@gmail.com>
2022-09-23 10:01:57 +0700

    routers

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
+const router = require('./routers');
 const pool = require('./config/DBConnection');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hola');
-});
+app.use(router);
 
 app.listen(port, () => {
   console.log(`server listening on http://localhost:${port}`);
