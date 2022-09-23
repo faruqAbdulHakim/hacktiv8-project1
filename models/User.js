e5433cf640e397fd293e0bcc94743677ce64c3e2
perdanaph <danangputro9@gmail.com>
2022-09-23 23:17:01 +0700

    add: login model

--- models/User.js
+++ models/User.js
@@ -14,10 +14,23 @@ class User {
       const register = await pool.query(`
         INSERT INTO users (email, password) 
           VALUES ($1, $2) 
-          RETURNING email, password;`, 
-        [email, hashing]
+          RETURNING email, password;`, [email, hashing]
       );
       return new User(register.rows[0].id, register.rows[0].email, register.rows[0].password)
+    } catch (error) {
+      return { success: false, error }
+    }
+  }
+
+  static async login(email, password) {
+    try {
+      const login = await pool.query(`
+      SELECT id, email, password 
+        FROM users WHERE email = $1;`,[email]
+      );
+      if (login.rows.length === 0) throw { msg: 'UserNotFound' };
+      if (!hashPassword.comparePassword(password, login.rows[0].password)) throw { msg: 'Wrong Password' }
+      return new User(login.rows[0].id, login.rows[0].email, login.rows[0].password)
     } catch (error) {
       return { success: false, error }
     }
