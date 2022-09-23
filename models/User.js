185d764ede816bd963a0e4e9b527f50ca6746366
perdanaph <danangputro9@gmail.com>
2022-09-24 00:04:12 +0700

    update validate user login

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const pool = require('./../config/DBConnection');
-const hashPassword = require('./../helpers/bcrypt');
+
 
 class User {
   constructor(id, email, password) {
@@ -28,8 +28,6 @@ class User {
       SELECT id, email, password 
         FROM users WHERE email = $1;`,[email]
       );
-      if (login.rows.length === 0) throw { msg: 'UserNotFound' };
-      if (!hashPassword.comparePassword(password, login.rows[0].password)) throw { msg: 'Wrong Password' }
       return new User(login.rows[0].id, login.rows[0].email, login.rows[0].password)
     } catch (error) {
       return { success: false, error }
