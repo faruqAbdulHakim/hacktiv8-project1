fa8acb7e882db6f82e4dc5fc1f66aa8902c0571d
perdanaph <danangputro9@gmail.com>
2022-09-23 23:32:16 +0700

    add: login controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,7 +6,16 @@ exports.register = async (req, res) => {
     const register = await User.register(email, password);
     res.status(201).json({ message: 'Success', account: register });
   } catch (error) {
-    console.log(error)
     res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const login = await User.login(email, password);
+    res.status(200).json({ message: 'Success, You are login', login })
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' })
   }
 }
