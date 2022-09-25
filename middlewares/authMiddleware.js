7e2bbc0cbd87ad34228d6b94484450a1724d1fdb
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-09-25 18:56:47 +0700

    update: authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,18 +1,27 @@
-const { verify } = require('../helpers/jwtHelper')
+const { verify } = require('../helpers/jwtHelper');
 
-function userVerify (req, res, next) {
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {import('express').NextFunction} next
+ */
+function authMiddleware(req, res, next) {
   try {
-    const authHeader = req.headers['authorization']
-    token = authHeader.split("Bearer ");
-    if (!authHeader) throw { name: 'notLogin'};
-    if (token.length !== 2) throw { name: 'invalidToken'};
-    const { id, email } = verify(token[1]);
-    if (!user) throw { name: 'Unauthorized' };
-    req.user = { id, email };
-    next()
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) throw { name: 'Unauthorized' };
+
+    token = authHeader.split('Bearer ');
+    if (token.length !== 2) throw { name: 'InvalidToken' };
+
+    const { id } = verify(token[1]);
+    if (!id) throw { name: 'Unauthorized' };
+
+    req.user = { id };
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
-module.exports = userVerify;
+module.exports = authMiddleware;
--- routers/index.js
+++ routers/index.js
@@ -1,15 +1,14 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const usersRouter = require('./usersRouter.js');
 const reflectionsRouter = require('./reflectionsRouter.js');
-const userVerify = require('../middlewares/authMiddleware.js')
+const authMiddleware = require('../middlewares/authMiddleware.js');
 const errorMiddleware = require('../middlewares/errorMiddleware.js');
 
-router.use('/api/v1/users',usersRouter);
-router.use(userVerify);
-router.use('/api/v1/reflections',reflectionsRouter);
+router.use('/api/v1/users', usersRouter);
+router.use('/api/v1/reflections', authMiddleware, reflectionsRouter);
 
 router.use((req, res, next) => {
-    next({ name: 'PageNotFound' });
+  next({ name: 'PageNotFound' });
 });
 
 router.use(errorMiddleware);
