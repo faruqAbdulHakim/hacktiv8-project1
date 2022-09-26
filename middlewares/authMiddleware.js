33b6126fecf4c8b07989d476538a3e150cedbf24
perdanaph <danangputro9@gmail.com>
2022-09-26 15:46:33 +0700

    update: validation and fix token payload

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -9,15 +9,15 @@ const { verify } = require('../helpers/jwtHelper');
 function authMiddleware(req, res, next) {
   try {
     const authHeader = req.headers['authorization'];
-    if (!authHeader) throw { name: 'Unauthorized' };
+    if (!authHeader) throw { name: 'notLogin' };
 
-    token = authHeader.split('Bearer ');
+    token = authHeader.split(' ')[1];
     if (token.length !== 2) throw { name: 'InvalidToken' };
 
-    const { id } = verify(token[1]);
+    const { id, email } = verify(token[1]);
     if (!id) throw { name: 'Unauthorized' };
 
-    req.user = { id };
+    req.user = { id, email };
     next();
   } catch (error) {
     next(error);
