1af9e4e6a391b62bf4039438891479503dbe6986
perdanaph <danangputro9@gmail.com>
2022-09-27 20:18:12 +0700

    update: merapikan code

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -14,19 +14,13 @@ class UsersController {
       const { email, password } = req.body;
       const cekEmail = await User.login(email, password);
 
-      if (email === cekEmail.email) {
-        throw { name: 'EmailExist' };
-      }
+      if (email === cekEmail.email) throw { name: 'EmailExist' };
 
-      if (password.length < 8) {
-        throw { name: 'reqPassword' };
-      }
+      if (password.length < 8) throw { name: 'reqPassword' };
 
       const register = await User.register(email, password);
-      res.status(201).json({
-        message: 'Register Success',
-        account: register.email,
-      });
+
+      res.status(201).json({ message: 'Register Success', account: register.email });
     } catch (error) {
       next(error);
     }
@@ -43,13 +37,9 @@ class UsersController {
       const { email, password } = req.body;
       const login = await User.login(email, password);
 
-      if (!login.email) {
-        throw { name: 'UserNotFound' };
-      }
+      if (!login.email) throw { name: 'UserNotFound' };
 
-      if (!bcrypt.comparePassword(password, login.password)) {
-        throw { name: 'WrongPassword' };
-      }
+      if (!bcrypt.comparePassword(password, login.password)) throw { name: 'WrongPassword' };
 
       const token = sign(
         {
